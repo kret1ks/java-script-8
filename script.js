bf5b76d7abf47f3d47682c19993d5c547166f6db
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-08-29 21:08:06 +0300

    123

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
 // 1. Напиши скрипт, який об'єднує всі елементи массива в один рядок. Елементів може бути довільна кількість. Нехай елементи массива в рядку будут розділені комою.
 
 
-const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+// const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
 
-let string = '';
+// let string = '';
 
-for(let i = 0; i < friends.length; i += 1){
-    string += friends[i]
+// for(let i = 0; i < friends.length; i += 1){
+//     string += friends[i]
 
-    if(i < friends.length -1){
-        string += ", ";
-    }
-}
-console.log(friends);
+//     if(i < friends.length -1){
+//         string += ", ";
+//     }
+// }
+// console.log(friends);
+
+
+const array = ['яблуко', 'банан', 'груша', 'апельсин'];
+
+const result = array.join(', '); 
+
+console.log(result); 
+
+
+
+
+
+
+// 2. Працюємо з колекцією карток в trello. Метод splice() (можна використати інші методи)
+
+const cards = [
+  'Карточка-1',
+  'Карточка-2',
+  'Карточка-3',
+  'Карточка-4',
+  'Карточка-5',
+];
+
+
+const copyCards = cards.slice()
+
+const resultat = copyCards.splice(0, 2)
+console.log(copyCards);
+console.log(resultat);
+
